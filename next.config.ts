f6587953a635b5fb85bbae4b905984c0412bbf38
xiaowangovo-001 <xiaowangovo-001@github.com>
2026-07-15 21:55:12 +0800

    🔧 重命名项目为 xiaozhuangovo

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/xinqingliaoyu',
-  assetPrefix: '/xinqingliaoyu',
+  basePath: '/xiaozhuangovo',
+  assetPrefix: '/xiaozhuangovo',
 };
 
 export default nextConfig;
