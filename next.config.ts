56e35d3cd94cfdd364d9e512536e4a7dc89b5329
xiaowangovo-001 <xiaowangovo-001@github.com>
2026-07-15 22:26:06 +0800

    🔧 修正仓库名

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/-xinqingliaoyu',
-  assetPrefix: '/-xinqingliaoyu',
+  basePath: '/xinqingliaoyu',
+  assetPrefix: '/xinqingliaoyu',
 };
 
 export default nextConfig;
